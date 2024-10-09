d21d2aa955f5f4c3ea01881daacf2557693e49c4
arthur-ngdi <arthur.f@gec.inatel.br>
2024-10-09 19:14:14 -0300

    Criação da rota da view de projects

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,5 @@
+<x-layouts.app>
+    <a href="{{route('project.show(1))}}">
+
+    </a>
+</x-layouts.app>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::view('/', 'projects.index');
+
+Route::view('/project/{project}', 'projects.show')->name('projects.show');
